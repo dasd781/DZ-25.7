aafae7d8c6852a09611ec87e714b327d0f3cbbe7
Andrei <dasd_78@mail.ru>
2023-07-12 20:59:08 +0300

    dz 15.7

---
+++ DZ-25.7/DZ-25.7/BadLogin.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <exception>
+
+class BadLogin : public std::exception
+{
+public:
+	virtual const char* what() const noexcept override;
+};
---
+++ DZ-25.7/DZ-25.7/BadPassword.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <exception>
+
+class BadPassword : public std::exception
+{
+public:
+	virtual const char* what() const noexcept override;
+};
